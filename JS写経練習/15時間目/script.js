685e2399cc904b171eed1a8f4bd3f5b8b0e7dcc1
naoyamamoto108 <naocchiiii.108@gmail.com>
2024-03-28 23:08:30 +0900

    test version02

---
+++ JS写経練習/15時間目/script.js
@@ -0,0 +1,51 @@
+function fruitsPrice(apple,orange){
+ return(apple*80)+(orange*60);
+}
+  document.write(fruitsPrice(3,5)+"円"+"<br>");
+  document.write(fruitsPrice(6,8)+"円"+"<br>");
+
+document.write("<br>");
+
+function nameHello(name){
+ return name+"さん。こんにちは";
+}
+  document.write(nameHello("高橋"));
+  document.write("<br>");
+  document.write(nameHello("鈴木"));
+  document.write("<br>");
+
+document.write("<br>");
+
+function juusho(abc){
+ return abc+"出身の方";
+}
+  document.write(juusho("東京"));
+  document.write("<br>");
+  document.write(juusho("神奈川"));
+  document.write("<br>");
+  document.write(juusho("大阪"));
+  document.write("<br>");
+
+document.write("<br>");
+
+function test(num){
+ return"あなたの点数は"+num+"点です。";
+}
+  document.write(test(90));
+  document.write("<br>");
+  document.write(test(88));
+  document.write("<br>");
+  document.write(test(79));
+  document.write("<br>");
+
+document.write("<br>");
+
+function oshirase(num){
+ return"会員Noは"+num+"です。";
+}
+  document.write(oshirase(1022));
+  document.write("<br>")
+  document.write(oshirase(1199));
+  document.write("<br>")
+  document.write(oshirase(2047));
+  document.write("<br>")
---
+++ JS写経練習/4時間目/script.js
@@ -0,0 +1,42 @@
+var a="山本";
+document.write(a);
+
+document.write("<br>");
+
+var number=208;
+document.write(number);
+
+document.write("<br>");
+
+var tell="090-0000-1234";
+document.write(tell);
+
+document.write("<br>");
+
+var name="Jody";
+document.write(name);
+
+document.write("<br>");
+
+var name2="高橋さんと田中さん";
+document.write(name2);
+
+document.write("<br>");
+
+var cost="2001円";
+document.write(cost);
+
+document.write("<br>");
+
+var month="12";
+document.write(month+"月");
+
+document.write("<br>")
+
+var day=25;
+document.write(day+"日");
+
+document.write("<br>")
+
+var youbi="木";
+document.write(youbi+"曜日");
